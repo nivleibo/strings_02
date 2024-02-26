d2516dfa5ce00cc2e927ab7c37a5586d1352f63a
Niv Leibowitz <nl7881@princeton.edu>
2024-02-25 21:43:12 -0500

    ff

--- str.h
+++ str.h
@@ -1,12 +1,17 @@
+
+#ifndef STR_MODULE
+#define STR_MODULE
+
 #include <stddef.h>
 
+size_t Str_getLength(const char *pcSrc);
 
-size_t Str_getLength(const char pcSrc[]) ;
+char *Str_copy(char *dest, const char *src);
 
-char *Str_copy(char dest[], const char src[]);
+char* Str_concat (  char *destination,const char *source);
 
-char* Str_concat (  char destination[],const char source[]);
+int Str_compare(const char *str1, const char * str2);
 
-int Str_compare(const char str1[], const char str2[]);
+char* Str_search (const char *haystack, const char *needle);
 
-char* Str_search (const char haystack[], const char needle[]);
+#endif
